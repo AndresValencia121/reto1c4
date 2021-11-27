ea90096a68162b0cdcd614c468d0d7c135899c30
unknown <andres.valencia121@gmail.com>
2021-11-27 17:31:04 -0500

    first commit

---
+++ src/main/java/mintic/reto1/Controller/UserController.java
@@ -0,0 +1,59 @@
+package mintic.reto1.Controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+// import java.util.Optional;
+
+import mintic.reto1.Service.UserService;
+import mintic.reto1.Model.User;
+
+
+@RestController
+@RequestMapping("/api/user")
+
+@CrossOrigin(origins="*", methods = {RequestMethod.GET,RequestMethod.PUT,RequestMethod.POST,RequestMethod.DELETE})//responde peticiones desde cualquier lado
+public class UserController {
+    
+    @Autowired
+    private UserService UserService;
+
+    @GetMapping("/all")
+    public List<User> getUsers(){
+        return UserService.getAll();
+    }
+
+
+    @GetMapping("/{user_email}")
+    public boolean getByUserEmail(@PathVariable("user_email") String email){//toma como variable lo que llega en la ruta
+        
+            if(UserService.getByEmail(email)== null){
+                return false;
+            }
+            return true;
+       
+    }
+
+    @GetMapping("/{user_email}/{user_password}")
+    public User getByUserEmailAndUserPassword(@PathVariable("user_email") String email, @PathVariable("user_password") String password){//toma como variable lo que llega en la ruta
+        return UserService.getByEmailAndPassword(email,password);
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)//status 201
+    public User save(@RequestBody User User){ //es pra  que los parametros del json lleguen bien como un modelo
+        return UserService.save(User);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)//status 201
+    public String deleteUser(@PathVariable("id") int id){
+        UserService.deleteUser(id);
+        return "redirect:/";
+    }
+
+
+}
